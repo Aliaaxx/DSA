b012a58750c0e514e1fe41eca1dba0311dc673b3
Aliaaxx <125764832+Aliaaxx@users.noreply.github.com>
2024-08-14 21:16:28 +0300

    Create For_each_number_in_B_count_the_number_that_less_than_it_in_A.cpp

---
+++ Two_Pointers_&_Sliding_Window/For_each_number_in_B_count_the_number_that_less_than_it_in_A.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define ll long long
+#define IO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+using namespace std;
+
+int main() {
+    IO;
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n), b(m), ans(m);
+    for(int &i : a) cin >> i;
+    for(int &i : b) cin >> i;
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+
+    int i = 0, j = 0, count = 0;
+    while (j < m) {
+        if (i == n) {
+            ans[j] = count;
+            j++;
+        }
+        else {
+            if (a[i] < b[j]) {
+                count += 1;
+                i++;
+            }
+            else {
+                ans[j] = count;
+                j++;
+            }
+        }
+    }
+    
+    for (int x : ans) {
+        cout << x << " ";
+    }
+    return 0;
+}
