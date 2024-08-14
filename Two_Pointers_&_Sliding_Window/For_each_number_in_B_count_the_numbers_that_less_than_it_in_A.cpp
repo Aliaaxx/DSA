5d39035c4dfdf0726d0fb23f6252dc28d157dbfe
Aliaaxx <125764832+Aliaaxx@users.noreply.github.com>
2024-08-15 00:39:13 +0300

    Update For_each_number_in_B_count_the_numbers_that_less_than_it_in_A.cpp

--- Two_Pointers_&_Sliding_Window/For_each_number_in_B_count_the_numbers_that_less_than_it_in_A.cpp
+++ Two_Pointers_&_Sliding_Window/For_each_number_in_B_count_the_numbers_that_less_than_it_in_A.cpp
@@ -6,6 +6,39 @@ using namespace std;
 
 int main() {
     IO;
+    int n, target;
+    cin >> n >> target;
+    vector<int> arr(n);
+    for (int &x : arr) cin >> x;
+    int i = 0, j = 0, sum = 0;
+    while (i < n) {
+        while (j < n && sum < target) {
+            sum += arr[j];
+            j++;
+        }
+        if (sum == target) {
+            cout << i << " " << j - 1 << endl;
+            return 0;
+        }
+        sum -= arr[i];
+        i++;
+    }
+    cout <<  -1 << " " << -1 << endl;
+    return 0;
+}
+
+//////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
+
+#include <bits/stdc++.h>
+#define endl "\n"
+#define ll long long
+#define IO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+using namespace std;
+
+int main() {
+    IO;
     int n, m;
     cin >> n >> m;
     vector<int> a(n), b(m), ans(m);
