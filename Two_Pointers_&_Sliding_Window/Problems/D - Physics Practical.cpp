37537d6f53ef59b25d6c13a48b296b1a3481e8d2
Aliaaxx <125764832+Aliaaxx@users.noreply.github.com>
2024-08-20 02:10:24 +0300

    Create D - Physics Practical.cpp

---
+++ Two_Pointers_&_Sliding_Window/Problems/D - Physics Practical.cpp
@@ -0,0 +1,32 @@
+///*** https://codeforces.com/contest/253/problem/B ***///
+
+#include <bits/stdc++.h>
+#define endl "\n"
+#define ll long long
+#define IO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+using namespace std;
+ 
+int main() {
+    IO;
+    ifstream Cin ("input.txt");
+    ofstream Cout ("output.txt");
+    int n;
+    Cin >> n;
+    vector<int> v(n);
+    for(int &i: v) Cin >> i;
+    sort(v.begin(), v.end());
+    int i =0, j =0, mn = INT_MAX;
+    while (i < n) {
+        while (j < n and v[j] <= v[i] * 2) {
+            j++;
+        }
+        int cmp = n - (j - i);
+        mn = min(mn, cmp);
+        if (j == n and v[i] * 2 >= v[j]) {
+            break;
+        }
+        i++;
+    }
+    Cout << mn << endl;
+    return 0;
+}
