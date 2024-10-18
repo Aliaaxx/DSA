b538f5eaa78d3709b3fd0791d983fc0011b93652
Aliaaxx <125764832+Aliaaxx@users.noreply.github.com>
2024-10-18 19:51:24 +0300

    Update and rename Friends - fix it.cpp to Friends.cpp

--- Graphs/DSU/Problems/Friends.cpp
+++ Graphs/DSU/Problems/Friends.cpp
@@ -8,15 +8,15 @@ using namespace std;
 
 vector<int> id, sz;
 
-int fnd(int e) {
-    if (id[e] == e) return e;
-    else return id[e] = fnd(id[e]);
+int find(int e) {
+    if (id[e] == e) return e; // returns the representative
+    else return id[e] = find(id[e]); // path compression
 }
 
-bool uni(int u, int v) {
-    u = fnd(u), v = fnd(v); // representative of each node
-    if (u == v) return false; // ignore, they already connected
-    return id[u] = v, sz[v] += sz[u], sz[u] = 0, true; // connect them
+bool uni(int f, int s) {
+    f = find(f), s = find(s); // representative of each node
+    if (f == s) return false; // ignore, they already connected
+    return id[f] = s, sz[s] += sz[f], sz[f] = 0, true; // connect them
 }
 
 int main() {
@@ -27,8 +27,8 @@ int main() {
         int n, m;
         cin >> n >> m;
         id.resize(n);
-        sz.resize(n, 1);
         iota(id.begin(), id.end(), 0);
+        sz.assign(n, 1);
         for (int i = 0; i < m; ++i) {
             int e, v;
             cin >> e >> v;
