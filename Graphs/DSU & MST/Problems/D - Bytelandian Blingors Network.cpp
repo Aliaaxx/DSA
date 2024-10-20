3191ea520a90bc0b5369a3629180f89a0078fae2
Aliaaxx <125764832+Aliaaxx@users.noreply.github.com>
2024-10-20 17:03:09 +0300

    Create D - Bytelandian Blingors Network - fix.cpp

---
+++ Graphs/DSU & MST/Problems/D - Bytelandian Blingors Network.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define ll long long
+#define IO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+using namespace std;
+
+vector<int> par;
+
+int fnd(int ch) {
+    if (ch == par[ch]) return ch;
+    else return par[ch] = fnd(par[ch]);
+}
+
+bool uni(int f, int s) {
+    f = fnd(f), s = fnd(s);
+    if (f == s) return false;
+    return par[f] = s, true;
+}
+
+int main() {
+    IO;
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<array<int, 3>> edges;
+        par.resize(n);
+        iota(par.begin(), par.end(), 0);
+        for (int i = 0; i < n; ++i) {
+            string s;
+            cin >> s;
+            int nei;
+            cin >> nei;
+            while (nei--) {
+                int u, w;
+                cin >> u >> w;
+                u--;
+                edges.push_back({w, i, u});
+            }
+        }
+        sort (edges.begin(), edges.end());
+        vector<vector<pair<int, int>>> adj(n);
+        for (auto &[w, u, v] : edges) {
+            if (uni(u, v)) {
+                adj[u].push_back({v, w});
+                adj[v].push_back({u, w});
+            }
+        }
+        int src = 0;
+        queue<pair<int, int>> q;
+        vector<int> dis(n, -1);
+        q.push({src, INT_MAX});
+        dis[src] = 0;
+        while (!q.empty()) {
+            auto [cur, mx] = q.front();
+            q.pop();
+            for (auto &[node, w] : adj[cur]) {
+                if (dis[node] == -1) {
+                    dis[node] = min(mx, w);
+                    q.push({node, dis[node]});
+                }
+            }
+        }
+        int ans = 0;
+        for (int i : dis) {
+            ans +=  i;
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
