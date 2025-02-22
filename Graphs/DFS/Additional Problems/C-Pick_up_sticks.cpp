b7df4cc18bd6d41d570e07a873c4f0503e14e8a6
Aliaa Hamad <125764832+Aliaaxx@users.noreply.github.com>
2025-02-22 23:42:23 +0200

    Update C-Pick_up_sticks.cpp

--- Graphs/DFS/Additional Problems/C-Pick_up_sticks.cpp
+++ Graphs/DFS/Additional Problems/C-Pick_up_sticks.cpp
@@ -1,4 +1,66 @@
 ///*** https://vjudge.net/problem/UVA-11686 ***///
+#include <bits/stdc++.h>
+#define endl "\n"
+#define IO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+using namespace std;
+
+bool dfs(vector<vector<int>>& adj, vector<bool>& vis, vector<bool>& recStack, int cur, stack<int>& stk) {
+    vis[cur] = true;
+    recStack[cur] = true;
+    for (int i : adj[cur]) {
+        if (recStack[i]) return true;
+        if (!vis[i]) {
+            if (dfs(adj, vis, recStack, i, stk)) {
+                return true;
+            }
+        }
+    }
+    recStack[cur] = false;
+    stk.push(cur);
+    return false;
+}
+
+pair<bool, vector<int>> dfsHelper(vector<vector<int>>& adj, int n) {
+    vector<bool> vis(n, false), recStack(n, false);;
+    stack<int> stk;
+    vector<int> ans;
+    for (int i = 0; i < n; i++) {
+        if (!vis[i]) {
+            if (dfs(adj, vis, recStack, i, stk)){
+                return {true, ans};
+            }
+        }
+    }
+    while(!stk.empty()) {
+        ans.push_back(stk.top());
+        stk.pop();
+    }
+    return {false, ans};
+}
+
+int main() {
+    IO;
+    int n, m, a, b;
+    while(cin>>n>>m && n) {
+        vector<vector<int>> adj(n);
+        for (int i = 0; i < m; i++) {
+            cin >> a >> b;
+            a--;
+            b--;
+            adj[a].push_back(b);
+        }
+        auto [cycle, ans] = dfsHelper(adj, n);
+        if (cycle) cout << "IMPOSSIBLE" << endl;
+        else {
+            for (int i: ans) cout << i + 1 << endl;
+        }
+    }
+    return 0;
+}
+
+
+
+// Incorrect input for this question but it is the correct idea
 
 #include <bits/stdc++.h>
 #define endl "\n"
